c08de5349406f4205b5b9068e5abb5a633fc61c8
trippone trippini <visualtrippa@hotmail.com>
2022-12-02 19:55:49 +0100

    anagrafica

--- Program.cs
+++ Program.cs
@@ -4,9 +4,22 @@ namespace ConsoleApplicationsTDPC14
 {
     internal class Program
     {
+        /*
+         scrivere un programma che:
+            - chieda all'utente come si chiama
+            - chieda all'utente quanti anni ha
+            - stampi su schermo il nome dell'utente
+            - stampu su schermo l'età dell'utente
+         */
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //garbage collector
+            Console.WriteLine("Come ti chiami ?");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Quanti anni hai ?");
+            string eta = Console.ReadLine();
+            Console.WriteLine("Ti chiami: " + nome);
+            Console.WriteLine("Hai " + eta + " anni");
         }
     }
 }
