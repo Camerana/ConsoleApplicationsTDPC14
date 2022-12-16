0960580a7d6c2327439267739069bcec56cda5e7
trippone trippini <visualtrippa@hotmail.com>
2022-12-16 21:25:23 +0100

    es

--- Program.cs
+++ Program.cs
@@ -4,6 +4,25 @@ namespace ConsoleApplicationsTDPC14
 {
     internal class Program
     {
+        /*
+         - generare 5 numeri casuali compresi fra 0 e 20
+         - stampare su schermo le coppie di numeri uguali con il formato:
+            - es: "il numero1 e il numero2 sono uguali !" 
+         */
+        /*
+         - estrarre 5 numeri casuali compresi fra 1 e 20
+         - chiedere all'utente 10 numeri
+         - stampare su schermo se l'utente è riuscito a indovinare qualche numero
+           - es: "hai fatto ambo !", "hai fatto terna !".. etc..
+         */
+        /*
+         - creare due stringhe con valore arbitrario
+         - chiedere all'utente una terza stringa
+         - stampare su schermo le seguenti valutazioni:
+            - se la stringa dell'utente è uguale a una delle due variabili
+            - se la stringa dell'utente è di lunghezza maggiore o minore di ciascuna 
+            delle due variabili
+         */
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
