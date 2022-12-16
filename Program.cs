1de542f4486f8bc39437bb1bb3262ea4571a685c
trippone trippini <visualtrippa@hotmail.com>
2022-12-16 19:04:39 +0100

    if

--- Program.cs
+++ Program.cs
@@ -6,7 +6,24 @@ namespace ConsoleApplicationsTDPC14
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("inserisci un numero");
+            int numero = int.Parse(Console.ReadLine());
+            if (numero == 5)
+            {
+                Console.WriteLine("il numero è uguale a 5");
+            }
+            else if (numero == 10)
+            {
+                Console.WriteLine("il numero è uguale a 10");
+            }
+            else if (numero == 15)
+            {
+                Console.WriteLine("il numero è uguale a 15");
+            }
+            else
+            {
+                Console.WriteLine("non so ancora");
+            }
         }
     }
 }
