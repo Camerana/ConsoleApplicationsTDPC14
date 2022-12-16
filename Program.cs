25998432e51b3fab4e0a35d12d58f5e4a2400564
trippone trippini <visualtrippa@hotmail.com>
2022-12-16 21:18:57 +0100

    esercizi

--- Program.cs
+++ Program.cs
@@ -58,30 +58,75 @@ namespace ConsoleApplicationsTDPC14
          - se l'utente indovina uno dei due numeri casuali stampare su schermo:
             - hai indovinato il numero: [numero indovinato]
          */
+        //static void Main(string[] args)
+        //{
+        //    Random random = new Random();
+        //    int numero1 = random.Next(0, 11);
+        //    int numero2 = random.Next(0, 11);
+
+        //    if (numero1 > numero2)
+        //    {
+        //        Console.WriteLine("il primo è maggiore del secondo");
+        //    }
+        //    else if (numero1 < numero2)
+        //    {
+        //        Console.WriteLine("il secondo è maggiore del primo");
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("i numeri sono uguali");
+        //    }
+        //    Console.WriteLine("inserisci un numero tra 0 e 10");
+        //    int numero3 = int.Parse(Console.ReadLine());
+        //    //OR
+        //    if (numero3 == numero1 || numero3 == numero2)
+        //    {
+        //        Console.WriteLine("hai indovinato il numero: " + numero3);
+        //    }
+        //}
+        /*
+         - creare due stringhe con nomi "username" e "password" con valori arbitrari
+         - chiedere all'utente due input tramite i quali si deve loggare
+         - verificare la validità degli input dell'utente
+         - stampare su schermo se il login è riuscito o meno
+         - consiglio: l'AND si scrive "&&"
+         */
+        //static void Main(string[] args)
+        //{
+        //    string username = "user";
+        //    string password = "user123";
+        //    Console.WriteLine("Inserisci il nome utente");
+        //    string inputUsername = Console.ReadLine();
+        //    Console.WriteLine("Inserisci la password");
+        //    string inputPassword = Console.ReadLine();
+        //    if (inputUsername.ToLowerInvariant() == username.ToLowerInvariant()
+        //        && inputPassword == password)
+        //    {
+        //        Console.WriteLine("ti sei loggato");
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("login fallito");
+        //    }
+        //}
+        /*
+         - generare un numero casuale compreso tra 1 e 5
+         - chiedere all'utente un numero tra 1 e 10
+         - confrontare il numero dell'utente con una variabile int di valore arbitrario
+         e col numero casuale
+         - nel caso il numero dell'utente sia maggiore di 3 e uguale al numero casuale o alla variabile
+         stampare su schermo "hai vinto"
+         */
         static void Main(string[] args)
         {
             Random random = new Random();
-            int numero1 = random.Next(0, 11);
-            int numero2 = random.Next(0, 11);
-
-            if (numero1 > numero2)
+            int numeroCasuale = random.Next(1, 6);
+            Console.WriteLine("inserisci un numero tra 1 e 10");
+            int numeroUtente = int.Parse(Console.ReadLine());
+            int numeroArbitrario = 4;
+            if (numeroUtente > 3 && (numeroUtente == numeroCasuale || numeroUtente == numeroArbitrario))
             {
-                Console.WriteLine("il primo è maggiore del secondo");
-            }
-            else if (numero1 < numero2)
-            {
-                Console.WriteLine("il secondo è maggiore del primo");
-            }
-            else
-            {
-                Console.WriteLine("i numeri sono uguali");
-            }
-            Console.WriteLine("inserisci un numero tra 0 e 10");
-            int numero3 = int.Parse(Console.ReadLine());
-            //OR
-            if (numero3 == numero1 || numero3 == numero2)
-            {
-                Console.WriteLine("hai indovinato il numero: " + numero3);
+                Console.WriteLine("hai vinto");
             }
         }
     }
