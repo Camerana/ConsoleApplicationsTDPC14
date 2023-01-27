380fa7b8e09e8aa58e8a0f169592d4c8fe3be0b8
trippone trippini <visualtrippa@hotmail.com>
2023-01-27 19:46:36 +0100

    esempi

--- Program.cs
+++ Program.cs
@@ -4,41 +4,46 @@ namespace ConsoleApplicationsTDPC14
 {
     internal class Program
     {
+        //static void Main(string[] args)
+        //{
+
+        //}
+
         /*
          - chiedere all'utente 3 stringhe
          - stampare le 3 stringhe tutte in minuscolo e in maiuscolo
          - stampare i primi 3 caratteri di ciascuna stringa
             - attenzione alle stringhe di lunghezza minore di 3 caratteri
          */
-        static void Main(string[] args)
-        {
-            Console.WriteLine("inserisci la stringa 1");
-            string stringa1 = Console.ReadLine();
-            Console.WriteLine("inserisci la stringa 2");
-            string stringa2 = Console.ReadLine();
-            Console.WriteLine("inserisci la stringa 3");
-            string stringa3 = Console.ReadLine();
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine("inserisci la stringa 1");
+        //    string stringa1 = Console.ReadLine();
+        //    Console.WriteLine("inserisci la stringa 2");
+        //    string stringa2 = Console.ReadLine();
+        //    Console.WriteLine("inserisci la stringa 3");
+        //    string stringa3 = Console.ReadLine();
 
-            Console.WriteLine("stringa 1:");
-            Console.WriteLine(stringa1.ToLowerInvariant());
-            Console.WriteLine(stringa1.ToUpperInvariant());
-            if (stringa1.Length >= 3)
-                Console.WriteLine(stringa1.Substring(0, 3));
-            Console.WriteLine("----------");
+        //    Console.WriteLine("stringa 1:");
+        //    Console.WriteLine(stringa1.ToLowerInvariant());
+        //    Console.WriteLine(stringa1.ToUpperInvariant());
+        //    if (stringa1.Length >= 3)
+        //        Console.WriteLine(stringa1.Substring(0, 3));
+        //    Console.WriteLine("----------");
 
-            Console.WriteLine("stringa 2:");
-            Console.WriteLine(stringa2.ToLowerInvariant());
-            Console.WriteLine(stringa2.ToUpperInvariant());
-            if (stringa2.Length >= 3)
-                Console.WriteLine(stringa2.Substring(0, 3));
-            Console.WriteLine("----------");
+        //    Console.WriteLine("stringa 2:");
+        //    Console.WriteLine(stringa2.ToLowerInvariant());
+        //    Console.WriteLine(stringa2.ToUpperInvariant());
+        //    if (stringa2.Length >= 3)
+        //        Console.WriteLine(stringa2.Substring(0, 3));
+        //    Console.WriteLine("----------");
 
-            Console.WriteLine("stringa 3:");
-            Console.WriteLine(stringa3.ToLowerInvariant());
-            Console.WriteLine(stringa3.ToUpperInvariant());
-            if (stringa3.Length >= 3)
-                Console.WriteLine(stringa3.Substring(0, 3));
-        }
+        //    Console.WriteLine("stringa 3:");
+        //    Console.WriteLine(stringa3.ToLowerInvariant());
+        //    Console.WriteLine(stringa3.ToUpperInvariant());
+        //    if (stringa3.Length >= 3)
+        //        Console.WriteLine(stringa3.Substring(0, 3));
+        //}
 
         /*
          - generare un numero casuale compreso fra 1 e 10
@@ -61,29 +66,36 @@ namespace ConsoleApplicationsTDPC14
         //}
 
 
-        //static void Main(string[] args)
-        //{
-        //    string stringa = "questa è una stringa";
+        //ESEMPI
+        static void Main(string[] args)
+        {
+            string stringa = "questa è una stringa";
 
-        //    string stringa2 = stringa.Substring(5);
-        //    string stringa3 = stringa.Substring(5, 10);
+            string stringa2 = stringa.Substring(5);
+            string stringa3 = stringa.Substring(5, 10);
 
-        //    string stringa4 = stringa.Replace("questa", "quella");
-        //    string stringa5 = stringa.ToUpperInvariant();
-        //    string stringa6 = stringa.ToLowerInvariant();
-        //    string stringa7 = stringa.Remove(5, 6);
-        //    string[] parole = stringa.Split(' ');
+            string stringa4 = stringa.Replace("questa", "quella");
+            string stringa5 = stringa.ToUpperInvariant();
+            string stringa6 = stringa.ToLowerInvariant();
+            string stringa7 = stringa.Remove(5, 6);
+            string[] parole = stringa.Split(' ');
+            bool contains = stringa.Contains("gegeasgae");
+            bool startsWith = stringa.StartsWith("questa");
+            bool endsWith = stringa.EndsWith("stringa");
 
-        //    Console.WriteLine(stringa);
-        //    //Console.WriteLine(stringa2);
-        //    //Console.WriteLine(stringa3);
-        //    //Console.WriteLine(stringa4);
-        //    //Console.WriteLine(stringa5);
-        //    //Console.WriteLine(stringa6);
-        //    //Console.WriteLine(stringa7);
-        //    //for (int i = 0; i < parole.Length; i++)
-        //    //    Console.WriteLine(parole[i]);
-        //    Console.ReadKey();
-        //}
+            Console.WriteLine(stringa);
+            //Console.WriteLine(stringa2);
+            //Console.WriteLine(stringa3);
+            //Console.WriteLine(stringa4);
+            //Console.WriteLine(stringa5);
+            //Console.WriteLine(stringa6);
+            //Console.WriteLine(stringa7);
+            //for (int i = 0; i < parole.Length; i++)
+            //    Console.WriteLine(parole[i]);
+            //Console.WriteLine(contains);
+            //Console.WriteLine(startsWith);
+            //Console.WriteLine(endsWith);
+            Console.ReadKey();
+        }
     }
 }
