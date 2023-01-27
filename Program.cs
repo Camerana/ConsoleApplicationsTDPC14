63a81db3bba5b6e07e0906d4c864cbf617665166
trippone trippini <visualtrippa@hotmail.com>
2023-01-27 18:56:17 +0100

    esempi operazioni con stringhe

--- Program.cs
+++ Program.cs
@@ -6,7 +6,27 @@ namespace ConsoleApplicationsTDPC14
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string stringa = "questa è una stringa";
+
+            string stringa2 = stringa.Substring(5);
+            string stringa3 = stringa.Substring(5, 10);
+
+            string stringa4 = stringa.Replace("questa", "quella");
+            string stringa5 = stringa.ToUpperInvariant();
+            string stringa6 = stringa.ToLowerInvariant();
+            string stringa7 = stringa.Remove(5, 6);
+            string[] parole = stringa.Split(' ');
+
+            Console.WriteLine(stringa);
+            //Console.WriteLine(stringa2);
+            //Console.WriteLine(stringa3);
+            //Console.WriteLine(stringa4);
+            //Console.WriteLine(stringa5);
+            //Console.WriteLine(stringa6);
+            //Console.WriteLine(stringa7);
+            //for (int i = 0; i < parole.Length; i++)
+            //    Console.WriteLine(parole[i]);
+            Console.ReadKey();
         }
     }
 }
