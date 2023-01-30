669913a0a3cc04c82f5bbb7ed7ddb428ffad2cc7
trippone trippini <visualtrippa@hotmail.com>
2023-01-30 19:12:50 +0100

    asd

--- Program.cs
+++ Program.cs
@@ -6,7 +6,33 @@ namespace ConsoleApplicationsTDPC14
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string input = Console.ReadLine();
+            Console.WriteLine("entro in print");
+            Print();
+            Console.WriteLine("sono uscito da print");
+           
+            GetValueString();
+            string valueString = GetValueString();
+
+            GetValueInt();
+            int valueInt = GetValueInt();
+
+            Console.WriteLine(valueString);
+            Console.WriteLine(valueInt);
+        }
+        static void Print()
+        {
+            Console.WriteLine("sono Print");
+        }
+        static string GetValueString()
+        {
+            Console.WriteLine("sono GetValueString");
+            return "value";
+        }
+        static int GetValueInt()
+        {
+            Console.WriteLine("sono GetValueInt");
+            return 10;
         }
     }
 }
