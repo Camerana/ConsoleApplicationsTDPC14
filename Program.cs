9794e2e5ab96556175b0bc9d22941d0d740db374
trippone trippini <visualtrippa@hotmail.com>
2023-01-30 19:48:45 +0100

    asd

--- Program.cs
+++ Program.cs
@@ -9,6 +9,25 @@ namespace ConsoleApplicationsTDPC14
          all'utente i suoi dati
          anagrafici e stamparli su schermo (senza return)
          */
+        //static void Main(string[] args)
+        //{
+        //    GetNome();
+        //    GetCognome();
+        //}
+        //static void GetNome()
+        //{
+        //    Console.WriteLine("inserisci il tuo nome");
+        //    string input = Console.ReadLine();
+        //    Console.WriteLine(input);
+        //}
+        //static void GetCognome()
+        //{
+        //    Console.WriteLine("inserisci il tuo cognome");
+        //    string input = Console.ReadLine();
+        //    Console.WriteLine(input);
+        //}
+
+
         /*
          tramite due funzioni "GetNome" e "GetCognome" chiedere 
          all'utente i suoi dati
@@ -17,40 +36,60 @@ namespace ConsoleApplicationsTDPC14
          */
         static void Main(string[] args)
         {
-            string v = Funzione();
-
+            string nome = GetNome();
+            Console.WriteLine(nome);
+            string cognome = GetCognome();
+            Console.WriteLine(cognome);
+        }
+        static string GetNome()
+        {
+            Console.WriteLine("inserisci il tuo nome");
             string input = Console.ReadLine();
-            Console.WriteLine("entro in print");
-            Print();
-            Console.WriteLine("sono uscito da print");
+            return input;
+        }
+        static string GetCognome()
+        {
+            Console.WriteLine("inserisci il tuo cognome");
+            string input = Console.ReadLine();
+            return input;
+        }
 
-            GetValueString();
-            string valueString = GetValueString();
+        //static void Main(string[] args)
+        //{
+        //    string v = Funzione();
 
-            GetValueInt();
-            int valueInt = GetValueInt();
+        //    string input = Console.ReadLine();
+        //    Console.WriteLine("entro in print");
+        //    Print();
+        //    Console.WriteLine("sono uscito da print");
 
-            Console.WriteLine(valueString);
-            Console.WriteLine(valueInt);
-        }
-        static string Funzione()
-        {
-            string valore = "valore Funzione";
-            return valore;
-        }
-        static void Print()
-        {
-            Console.WriteLine("sono Print");
-        }
-        static string GetValueString()
-        {
-            Console.WriteLine("sono GetValueString");
-            return "value";
-        }
-        static int GetValueInt()
-        {
-            Console.WriteLine("sono GetValueInt");
-            return 10;
-        }
+        //    GetValueString();
+        //    string valueString = GetValueString();
+
+        //    GetValueInt();
+        //    int valueInt = GetValueInt();
+
+        //    Console.WriteLine(valueString);
+        //    Console.WriteLine(valueInt);
+        //}
+        //static string Funzione()
+        //{
+        //    string valore = "valore Funzione";
+        //    return valore;
+        //}
+        //static void Print()
+        //{
+        //    Console.WriteLine("sono Print");
+        //}
+        //static string GetValueString()
+        //{
+        //    Console.WriteLine("sono GetValueString");
+        //    return "value";
+        //}
+        //static int GetValueInt()
+        //{
+        //    Console.WriteLine("sono GetValueInt");
+        //    return 10;
+        //}
     }
 }
