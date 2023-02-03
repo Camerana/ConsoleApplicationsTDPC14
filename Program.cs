a76f21f38c72fe648b10ef74789269c2868bbb17
trippone trippini <visualtrippa@hotmail.com>
2023-02-03 18:51:37 +0100

    asd

--- Program.cs
+++ Program.cs
@@ -4,9 +4,39 @@ namespace ConsoleApplicationsTDPC14
 {
     internal class Program
     {
+        /*
+         creare due funzioni con queste caratteristiche:
+            1)- accetti in ingresso una stringa e un numero intero
+              - stampi la concatenazione della stringa e del numero
+              - stampi il numero al quadrato
+            2)- accetti in ingresso due booleani
+              - stampi su schermo se entrambi i booleani sono true
+         */
+        /*
+         - tramite una funzione generare 3 numeri casuali compresi
+            tra 0 e 10
+         - tramite una seconda funzione stampare su schermo la somma
+         e il prodotto dei tre numeri
+         */
+        /*
+         creare una funzione che:
+            - accetti in ingresso 2 stringhe
+            - stampi su schermo quella di lunghezza maggiore
+            - stampi su schermo se la prima contiene la seconda
+         */
+        /*
+         - chiedere all'utente 2 stringhe e un numero
+         - tramite una funzione stampare su schermo il numero moltiplicato
+         per 10 e per 20
+         - tramite una seconda funzione stampare su schermo il 5° carattere
+         della prima stringa
+         - tramite una terza funzione stampare su schermo la lettera finale
+         della seconda stringa
+         */
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+
         }
     }
 }
