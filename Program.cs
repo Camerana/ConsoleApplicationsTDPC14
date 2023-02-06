6dee989b2d320b0f7c8f170bcc26625a283b7393
trippone trippini <visualtrippa@hotmail.com>
2023-02-06 21:16:59 +0100

    asd

--- Program.cs
+++ Program.cs
@@ -5,20 +5,85 @@ namespace ConsoleApplicationsTDPC14
     internal class Program
     {
         /*
+         - creare due array di numeri interi
+         - popolare il primo array di numeri casuali
+         - copiare tutti gli elementi del primo array nel
+         secondo
+         - creare un terzo array che contenga gli elementi
+         di entrambi gli array
+         - stampare gli elementi di tutti e tre gli array
+         */
+        static void Main(string[] args)
+        {
+            int dimension = 10;
+            int[] numbers1 = new int[dimension];
+            int[] numbers2 = new int[dimension];
+
+            Random random = new Random();
+            for (int i = 0; i < dimension; i++)
+                numbers1[i] = random.Next();
+
+            for (int i = 0; i < dimension; i++)
+                numbers2[i] = numbers1[i];
+
+            int totalDimension = dimension * 2;
+            int[] numbers3 = new int[totalDimension];
+
+            for (int i = 0; i < dimension; i++)
+                numbers3[i] = numbers1[i];
+
+            for (int i = 0; i < dimension; i++)
+                numbers3[dimension + i] = numbers2[i];
+
+            PrintArray(numbers1);
+            PrintArray(numbers2);
+            PrintArray(numbers3);
+        }
+        static void PrintArray(int[] numeri)
+        {
+            for (int i = 0; i < numeri.Length; i++)
+                Console.WriteLine(numeri[i]);
+        }
+        /*
          - creare un array di stringhe di dimensione 5
          - popolare l'array con stringhe arbitrarie
          - chiedere all'utente quale stringa vuole stampare
          - stampare su schermo la stringa scelta dall'utente
          */
+        //static void Main(string[] args)
+        //{
+        //    string[] stringhe = new string[5];
+        //    for (int i = 0; i < stringhe.Length; i++)
+        //        stringhe[i] = "string numero " + i;
+        //    Console.WriteLine("quale stringa vuoi stampare ?");
+        //    int input = int.Parse(Console.ReadLine());
+        //    if (input >= 0 && input < stringhe.Length)
+        //        Console.WriteLine(stringhe[input]);
+        //}
         /*
          - creare una funzione che restituisca un array
          di numeri casuali
          - chiamare la funzione 2 volte e stampare tutti
          i numeri che restituisce
          */
-        static void Main(string[] args)
-        {
-        }
+        //static void Main(string[] args)
+        //{
+        //    for (int i = 0; i < 2; i++)
+        //    {
+        //        int[] numbers = GetNumbersArray();
+        //        for (int j = 0; j < numbers.Length; j++)
+        //            Console.WriteLine(numbers[j]);
+        //    }
+        //}
+        //static int[] GetNumbersArray()
+        //{
+        //    Random random = new Random();
+        //    int dimension = random.Next(5, 20);
+        //    int[] numbers = new int[dimension];
+        //    for (int i = 0; i < dimension; i++)
+        //        numbers[i] = random.Next();
+        //    return numbers;
+        //}
         //static void Main(string[] args)
         //{
         //    int dimensione = 3;
