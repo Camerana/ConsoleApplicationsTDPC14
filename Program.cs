b773fbf360b9495bed48a8d6040722adffb26024
trippone trippini <visualtrippa@hotmail.com>
2023-02-06 19:47:24 +0100

    hffdh

--- Program.cs
+++ Program.cs
@@ -4,24 +4,39 @@ namespace ConsoleApplicationsTDPC14
 {
     internal class Program
     {
+        /*
+         - creare un array di stringhe di dimensione 5
+         - popolare l'array con stringhe arbitrarie
+         - chiedere all'utente quale stringa vuole stampare
+         - stampare su schermo la stringa scelta dall'utente
+         */
+        /*
+         - creare una funzione che restituisca un array
+         di numeri casuali
+         - chiamare la funzione 2 volte e stampare tutti
+         i numeri che restituisce
+         */
         static void Main(string[] args)
         {
-            int dimensione = 3;
-            int[] numeri = new int[dimensione];
-            numeri[0] = 12;
-            numeri[1] = 2;
-            numeri[2] = 10;
-
-            string[] stringhe = new string[dimensione];
-            stringhe[0] = "stringa1";
-            stringhe[1] = "stringa2";
-            stringhe[2] = "stringa3";
-
-            for (int i = 0; i < dimensione; i++)
-            {
-                Console.WriteLine(numeri[i]);
-                Console.WriteLine(stringhe[i]);
-            }
         }
+        //static void Main(string[] args)
+        //{
+        //    int dimensione = 3;
+        //    int[] numeri = new int[dimensione];
+        //    numeri[0] = 12;
+        //    numeri[1] = 2;
+        //    numeri[2] = 10;
+
+        //    string[] stringhe = new string[dimensione];
+        //    stringhe[0] = "stringa1";
+        //    stringhe[1] = "stringa2";
+        //    stringhe[2] = "stringa3";
+
+        //    for (int i = 0; i < dimensione; i++)
+        //    {
+        //        Console.WriteLine(numeri[i]);
+        //        Console.WriteLine(stringhe[i]);
+        //    }
+        //}
     }
 }
