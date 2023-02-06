cdf2ef4a303eaa5d9372364f5e8ab37450bf84f4
trippone trippini <visualtrippa@hotmail.com>
2023-02-06 19:44:12 +0100

    asd

--- Program.cs
+++ Program.cs
@@ -6,7 +6,22 @@ namespace ConsoleApplicationsTDPC14
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int dimensione = 3;
+            int[] numeri = new int[dimensione];
+            numeri[0] = 12;
+            numeri[1] = 2;
+            numeri[2] = 10;
+
+            string[] stringhe = new string[dimensione];
+            stringhe[0] = "stringa1";
+            stringhe[1] = "stringa2";
+            stringhe[2] = "stringa3";
+
+            for (int i = 0; i < dimensione; i++)
+            {
+                Console.WriteLine(numeri[i]);
+                Console.WriteLine(stringhe[i]);
+            }
         }
     }
 }
