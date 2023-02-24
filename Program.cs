9aec68e9c2a9578ae920f250078f70ddfb10be4b
trippone trippini <visualtrippa@hotmail.com>
2023-02-24 19:01:54 +0100

    asd

--- Program.cs
+++ Program.cs
@@ -7,20 +7,40 @@ namespace ConsoleApplicationsTDPC14
     {
         static void Main(string[] args)
         {
-            int[] numeri = new int[] {
+            int[] numbers = new int[] {
                 10,20,30
             };
-            List<string> stringhe = new List<string>() {
+            List<string> strings = new List<string>() {
                 "stringa 1", "stringa 2", "stringa 3"
             };
-            for (int i = 0; i < numeri.Length; i++)
-                Console.WriteLine(numeri[i]);
-            for (int i = 0; i < stringhe.Count; i++)
-                Console.WriteLine(stringhe[i]);
-            foreach (int numero in numeri)
+            for (int i = 0; i < numbers.Length; i++)
+                Console.WriteLine(numbers[i]);
+            for (int i = 0; i < strings.Count; i++)
+                Console.WriteLine(strings[i]);
+            foreach (int n in numbers)
+                Console.WriteLine(n);
+            foreach (string s in strings)
+                Console.WriteLine(s);
+
+
+            List<int> numbersList = new List<int>() {
+                10,20,30
+            };
+
+            PrintNumbersList(numbers);
+
+            PrintNumbersList(numbersList);
+        }
+
+        static void PrintNumbersList(int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+                Console.WriteLine(numbers[i]);
+        }
+        static void PrintNumbersList(List<int> numbers)
+        {
+            foreach (int numero in numbers)
                 Console.WriteLine(numero);
-            foreach (string stringa in stringhe)
-                Console.WriteLine(stringa);
         }
     }
 }
