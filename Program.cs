908ec94e9183e471191a2c52e067b8e5a089ad88
trippone trippini <visualtrippa@hotmail.com>
2023-02-24 19:05:45 +0100

    asd

--- Program.cs
+++ Program.cs
@@ -5,6 +5,30 @@ namespace ConsoleApplicationsTDPC14
 {
     internal class Program
     {
+        /*
+         - chiedere all'utente 3 stringhe
+         - inserire le stringhe in una lista
+         - passare la lista a una funzione
+         - tramite la funzione, con un foreach, stampare
+            il contenuto della lista
+         */
+        /*
+         - chiedere all'utente un booleano
+         - se il booleano è true, generare 4 numeri casuali,
+            inserirli in un array e stampare il contenuto
+            dell'array tramite un foreach
+         - se il booleano è false, chiedere all'utente 2 stringhe,
+            inserirle in una lista e stampare il contenuto
+            della lista tramite un for
+         */
+        /*
+         - creare un array di contenuto arbitrario
+         - creare una lista di contenuto arbitrario
+         - creare una lista il cui contenuto sia l'unione di quelli
+            dell'array e della prima lista
+         - tramite un foreach stampare il contenuto della seconda
+            lista
+         */
         static void Main(string[] args)
         {
             int[] numbers = new int[] {
