e8b64b33564498dd5c4139fbb8a023f034d1f6c5
trippone trippini <visualtrippa@hotmail.com>
2023-02-24 18:52:18 +0100

    esempio

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ConsoleApplicationsTDPC14
 {
@@ -6,7 +7,20 @@ namespace ConsoleApplicationsTDPC14
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] numeri = new int[] {
+                10,20,30
+            };
+            List<string> stringhe = new List<string>() {
+                "stringa 1", "stringa 2", "stringa 3"
+            };
+            for (int i = 0; i < numeri.Length; i++)
+                Console.WriteLine(numeri[i]);
+            for (int i = 0; i < stringhe.Count; i++)
+                Console.WriteLine(stringhe[i]);
+            foreach (int numero in numeri)
+                Console.WriteLine(numero);
+            foreach (string stringa in stringhe)
+                Console.WriteLine(stringa);
         }
     }
 }
