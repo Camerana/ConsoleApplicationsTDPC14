455c776711dc4d49813d644f2d1ad4ea638f35d3
trippone trippini <visualtrippa@hotmail.com>
2023-02-28 19:11:48 +0100

    initial

---
+++ Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApplicationsTDPC14
+{
+    internal class Product
+    {
+        public string Name { get; set; }
+        public int Cost { get; set; }
+        public string Manufacturer { get; set; }
+        public int Number { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ConsoleApplicationsTDPC14
 {
     internal class Program
     {
+        /*
+         - creare 100 oggetti che rappresentino
+         prodotti diversi con caratteristiche:
+            - nome
+            - costo
+            - produttore
+            - numero
+         */
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Product> products = new List<Product>();
+            for (int i = 0; i < 100; i++)
+            {
+                Product product = new Product();
+                product.Name = "nome " + i;
+                product.Manufacturer = "manufacturer " + i;
+                product.Cost = i;
+                product.Number = i * 10;
+
+                products.Add(product);
+            }
+            for (int i = 0; i < products.Count; i++)
+            {
+                Console.WriteLine(products[i].Name + " " + products[i].Cost);
+                Console.WriteLine(products[i].Manufacturer + " " + products[i].Number);
+                Console.WriteLine("-------------------------------");
+            }
         }
     }
 }
