7c0bd626c139849d19273ca23a4a27cb66b46a64
trippone trippini <visualtrippa@hotmail.com>
2023-02-28 19:56:56 +0100

    ex

--- Program.cs
+++ Program.cs
@@ -6,6 +6,17 @@ namespace ConsoleApplicationsTDPC14
     internal class Program
     {
         /*
+         - creare una classe Gioco con properties:
+            - public string Nome
+            - public int Anno
+         - creare 10 oggetti di tipo Gioco e
+         valorizzarne le properties
+         - tramite un ciclo stampare su schermo
+         le properties dei 10 oggetti
+         */
+
+
+        /*
          - creare 100 oggetti che rappresentino
          prodotti diversi con caratteristiche:
             - nome
@@ -13,25 +24,25 @@ namespace ConsoleApplicationsTDPC14
             - produttore
             - numero
          */
-        static void Main(string[] args)
-        {
-            List<Product> products = new List<Product>();
-            for (int i = 0; i < 100; i++)
-            {
-                Product product = new Product();
-                product.Name = "nome " + i;
-                product.Manufacturer = "manufacturer " + i;
-                product.Cost = i;
-                product.Number = i * 10;
+        //static void Main(string[] args)
+        //{
+        //    List<Product> products = new List<Product>();
+        //    for (int i = 0; i < 100; i++)
+        //    {
+        //        Product product = new Product();
+        //        product.Name = "nome " + i;
+        //        product.Manufacturer = "manufacturer " + i;
+        //        product.Cost = i;
+        //        product.Number = i * 10;
 
-                products.Add(product);
-            }
-            for (int i = 0; i < products.Count; i++)
-            {
-                Console.WriteLine(products[i].Name + " " + products[i].Cost);
-                Console.WriteLine(products[i].Manufacturer + " " + products[i].Number);
-                Console.WriteLine("-------------------------------");
-            }
-        }
+        //        products.Add(product);
+        //    }
+        //    for (int i = 0; i < products.Count; i++)
+        //    {
+        //        Console.WriteLine(products[i].Name + " " + products[i].Cost);
+        //        Console.WriteLine(products[i].Manufacturer + " " + products[i].Number);
+        //        Console.WriteLine("-------------------------------");
+        //    }
+        //}
     }
 }
