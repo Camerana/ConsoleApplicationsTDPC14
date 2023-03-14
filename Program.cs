a30f2b6265f84f66e9356eee4a1e53c3f342ce95
trippone trippini <visualtrippa@hotmail.com>
2023-03-14 19:38:30 +0100

    ex

--- Program.cs
+++ Program.cs
@@ -1,10 +1,20 @@
 ﻿using System;
-using System.Runtime.CompilerServices;
 
 namespace ConsoleApplicationsTDPC14
 {
     internal class Program
     {
+        /*
+         - creare una classe Prodotto con le seguenti properties:
+            - Nome
+            - Costo
+         - chiedere all'utente nome e costo di un prodotto e istanziarne
+         un oggetto
+         - fare in modo che si possano settare nomi solo di lunghezza
+         maggiore di 5 caratteri
+         - stampare su schermo i valori delle properties dell'oggetto
+         istanziato
+         */
         static void Main(string[] args)
         {
             User u = new User();
