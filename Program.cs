09149bcb5fc2e75f81f93995dd94d192b1bfb0f8
trippone trippini <visualtrippa@hotmail.com>
2023-03-17 19:24:56 +0100

    asd

--- Program.cs
+++ Program.cs
@@ -4,33 +4,51 @@ namespace ConsoleApplicationsTDPC14
 {
     internal class Program
     {
+        /*
+         * Aggiornamento alle 20.00
+         - creare una classe Veicolo con:
+            - properties:
+                - Model
+                - Plate
+                - MaxSpeed
+            - metodi con contenuto arbitrario:
+                - Start
+                - Stop
+         - creare 2 oggetti di tipo Veicolo
+         - valorizzare le properties dei due oggetti e
+         invocarne i metodi         
+         */
         static void Main(string[] args)
         {
-            string input = "questo è l'input";
-            string word = "l'input";
 
-            //Utilities u = new Utilities();
-            //u.StringContainsText(input, word);
-            bool contains = Utilities.StringContainsText(input, word);
-            int lenght = Utilities.GetStringLength(input);
-
-            Product product = new Product();
-            product.Name = "Acqua";
-            product.Cost = "1.5";
-            product.PrintData();
-
-            Product product1 = new Product();
-            product1.Name = "Biscotti";
-            product1.Cost = "2";
-            product1.PrintData();
-
-            Product product2 = new Product();
-            product2.Name = "Panino";
-            product2.Cost = "3";
-            product2.PrintData();
-            //Product.PrintData();
-            //product.ObjectsCount = 10;
-            //Product.ObjectsCount = 10;
         }
+        //static void Main(string[] args)
+        //{
+        //    string input = "questo è l'input";
+        //    string word = "l'input";
+
+        //    //Utilities u = new Utilities();
+        //    //u.StringContainsText(input, word);
+        //    bool contains = Utilities.StringContainsText(input, word);
+        //    int lenght = Utilities.GetStringLength(input);
+
+        //    Product product = new Product();
+        //    product.Name = "Acqua";
+        //    product.Cost = "1.5";
+        //    product.PrintData();
+
+        //    Product product1 = new Product();
+        //    product1.Name = "Biscotti";
+        //    product1.Cost = "2";
+        //    product1.PrintData();
+
+        //    Product product2 = new Product();
+        //    product2.Name = "Panino";
+        //    product2.Cost = "3";
+        //    product2.PrintData();
+        //    //Product.PrintData();
+        //    //product.ObjectsCount = 10;
+        //    //Product.ObjectsCount = 10;
+        //}
     }
 }
