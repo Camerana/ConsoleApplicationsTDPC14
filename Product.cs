c41609f0a9f1c29933a07c2245b2972f7691e44c
trippone trippini <visualtrippa@hotmail.com>
2023-03-17 19:20:41 +0100

    commenti

--- Product.cs
+++ Product.cs
@@ -4,15 +4,24 @@ using System.Text;
 
 namespace ConsoleApplicationsTDPC14
 {
+    //classe normale
     public class Product
     {
+        //field statico
         private static int ObjectsCount = 0;
+
+        //property normale
         public string Name { get; set; }
+        //property normale
         public string Cost { get; set; }
+        
+        //costruttore
         public Product()
         {
             Product.ObjectsCount++;
         }
+
+        //metodo normale
         public void PrintData()
         {
             Console.WriteLine(this.Name + ": " + this.Cost);
--- Utilities.cs
+++ Utilities.cs
@@ -4,12 +4,15 @@ using System.Text;
 
 namespace ConsoleApplicationsTDPC14
 {
+    //classe statica
     public static class Utilities
     {
+        //funzione statica
         public static bool StringContainsText(string input, string word)
         {
             return input.Contains(word);
         }
+        //funzione statica
         public static int GetStringLength(string input)
         {
             return input.Length;
