0e79612088dd29f3b6f8da4d13e3847a9a7cca19
trippone trippini <visualtrippa@hotmail.com>
2023-03-17 20:55:37 +0100

    animale

---
+++ Animale.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApplicationsTDPC14
+{
+    internal class Animale
+    {
+        public string Razza { get; set; }
+        public string Colore { get; set; }
+        public void Mangia()
+        {
+            Console.WriteLine(this.Razza + " " + this.Colore + " mangia");
+        }
+        public void FaiIlVerso()
+        {
+            Console.WriteLine(this.Razza + " " + this.Colore + " fa il verso");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,8 +4,39 @@ namespace ConsoleApplicationsTDPC14
 {
     internal class Program
     {
+        /*creare una classe Animale con
+            - properties:
+                - Razza
+                - Colore
+            - metodi:
+                - Mangia
+                - Fai il verso
+          - creare 3 oggetti di tipo Animale,
+            popolarne le properties e invocarne
+            i metodi
+         */
+        static void Main(string[] args)
+        {
+            Animale a1 = new Animale();
+            a1.Razza = "Coniglio";
+            a1.Colore = "Bianco";
+            a1.Mangia();
+            a1.FaiIlVerso();
+
+            Animale a2 = new Animale();
+            a2.Razza = "Gatto";
+            a2.Colore = "Arancione";
+            a2.Mangia();
+            a2.FaiIlVerso();
+
+            Animale a3 = new Animale();
+            a3.Razza = "Elefante";
+            a3.Colore = "Grigio";
+            a3.Mangia();
+            a3.FaiIlVerso();
+
+        }
         /*
-         * Aggiornamento alle 20.00
          - creare una classe Veicolo con:
             - properties:
                 - Model
@@ -18,10 +49,24 @@ namespace ConsoleApplicationsTDPC14
          - valorizzare le properties dei due oggetti e
          invocarne i metodi         
          */
-        static void Main(string[] args)
-        {
+        //static void Main(string[] args)
+        //{
+        //    Veicolo v1 = new Veicolo();
+        //    v1.Model = "Panda";
+        //    v1.Plate = "FG546TH";
+        //    v1.MaxSpeed = 100;
 
-        }
+        //    Veicolo v2 = new Veicolo();
+        //    v2.Model = "500";
+        //    v2.Plate = "GH451ZY";
+        //    v2.MaxSpeed = 90;
+
+        //    v1.Start();
+        //    v1.Stop();
+
+        //    v2.Start();
+        //    v2.Stop();
+        //}
         //static void Main(string[] args)
         //{
         //    string input = "questo è l'input";
