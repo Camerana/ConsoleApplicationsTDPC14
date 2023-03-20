d2956168bdad30bdd1a5ad374fc668908606ddef
trippone trippini <visualtrippa@hotmail.com>
2023-03-20 19:42:17 +0100

    esercizi

--- Program.cs
+++ Program.cs
@@ -4,14 +4,41 @@ namespace ConsoleApplicationsTDPC14
 {
     internal class Program
     {
+        /*
+         Simulare una lotteria:
+            - chiedere all'utente 3 numeri
+            - estrarre 5 numeri casuali
+            - stampare su schermo se l'utente ha indovinato
+            dei numeri e quali
+         */
+        /*
+         Simulare una lotteria con biglietti:
+            - creare una classe Giocatore con:
+                - properties:
+                    - lista di biglietti comprati
+            - creare una classe Biglietto con:
+                - properties:
+                    - Luogo di acquisto
+                    - Seriale
+            - permettere all'utente di comprare n biglietti
+            - creare una lista di biglietti estraibili
+            - estrarre un biglietto dalla lista e stampare 
+            su schermo i dati del biglietto estratto
+            - comunicare all'utente se ha vinto
+            - 
+         */
         static void Main(string[] args)
         {
-            Animale a = new Animale();
-            a.Razza = "Elefante";
-            a.Eta = 56;
 
-            Animale a1 = new Animale("Cavallo", 12);
         }
+        //static void Main(string[] args)
+        //{
+        //    Animale a = new Animale();
+        //    a.Razza = "Elefante";
+        //    a.Eta = 56;
+
+        //    Animale a1 = new Animale("Cavallo", 12);
+        //}
         //static void Main(string[] args)
         //{
         //    Prodotto p = new Prodotto();
