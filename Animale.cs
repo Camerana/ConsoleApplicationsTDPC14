a9d2bcba6a8a07293bfb9f4c142dba93db4c382d
trippone trippini <visualtrippa@hotmail.com>
2023-03-20 19:32:57 +0100

    asd

---
+++ Animale.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace ConsoleApplicationsTDPC14
+{
+    internal class Animale
+    {
+        public string Razza { get; set; }
+        public int Eta { get; set; }
+
+        public Animale()
+        {
+            Console.WriteLine("E' stato creato un animale");
+        }
+        public Animale(string razza, int eta)
+        {
+            this.Razza = razza;
+            this.Eta = eta;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,10 +6,18 @@ namespace ConsoleApplicationsTDPC14
     {
         static void Main(string[] args)
         {
-            Prodotto p = new Prodotto();
-            p.Nome = "Biscotti";
-            p.Costo = 15;
-            Console.WriteLine(p.Nome + ": " + p.Costo);
+            Animale a = new Animale();
+            a.Razza = "Elefante";
+            a.Eta = 56;
+
+            Animale a1 = new Animale("Cavallo", 12);
         }
+        //static void Main(string[] args)
+        //{
+        //    Prodotto p = new Prodotto();
+        //    p.Nome = "Biscotti";
+        //    p.Costo = 15;
+        //    Console.WriteLine(p.Nome + ": " + p.Costo);
+        //}
     }
 }
