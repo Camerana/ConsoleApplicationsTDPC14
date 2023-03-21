d559ef139b5cb4d8139ae7b940c1160c287bdd7f
trippone trippini <visualtrippa@hotmail.com>
2023-03-21 20:34:07 +0100

    asd

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ConsoleApplicationsTDPC14.Prodotti;
+using System;
 
 namespace ConsoleApplicationsTDPC14
 {
@@ -6,7 +7,34 @@ namespace ConsoleApplicationsTDPC14
     {
         static void Main(string[] args)
         {
+            Prodotto prodotto = new Prodotto();
+            prodotto.Nome = "Prodotto";
+            prodotto.Costo = "12";
+            prodotto.Stampa();
 
+            Biscotto biscotto = new Biscotto();
+            biscotto.Nome = "Biscotto";
+            biscotto.Costo = "3";
+            biscotto.Stampa();
+
+            Patatine patatine = new Patatine();
+            patatine.Nome = "Patatine";
+            patatine.Costo = "4";
+            patatine.Salinita = 2;
+            patatine.Stampa();
+
+            SanCarlo sanCarlo = new SanCarlo();
+            sanCarlo.Nome = "SanCarlo";
+            sanCarlo.Costo = "2";
+            sanCarlo.Salinita = 1;
+            sanCarlo.Stampa();
+
+            Pringles pringles = new Pringles();
+            pringles.Nome = "Pringles";
+            pringles.Costo = "2";
+            pringles.Salinita = 2;
+            pringles.AltezzaCilindro = 4;
+            pringles.Stampa();
         }
     }
 }
