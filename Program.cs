e646546b259b632271cbeeef86bf26f1fc8bc2ad
trippone trippini <visualtrippa@hotmail.com>
2023-03-24 19:10:14 +0100

    asd

--- Program.cs
+++ Program.cs
@@ -4,9 +4,27 @@ namespace ConsoleApplicationsTDPC14
 {
     internal class Program
     {
+        /* aggiornamento alle 7.40
+         creare una classe EssereVivente con
+            properties:
+                - specie
+                - nome
+            metodi:
+                - nutriti
+         creare una classe Vertebrato : EssereVivente con:
+            - metodi:
+                - nutriti
+                - muoviti
+         creare una classe Pianta : EssereVivente con:
+            - properties:
+                - profondità radici
+            - metodi:
+                - nutriti
+         - istanziare due oggetti di tipo Vertebrato e due di tipo Pianta,
+         valorizzarne le properties e invocarne i metodi
+         */
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
         }
     }
 }
