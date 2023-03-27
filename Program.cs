c8d6fac06db8b30d378f70ea98b5275377f2499f
trippone trippini <visualtrippa@hotmail.com>
2023-03-27 19:18:57 +0200

    aSD

--- Program.cs
+++ Program.cs
@@ -5,22 +5,44 @@ namespace ConsoleApplicationsTDPC14
 {
     internal class Program
     {
+        /*
+         creare una classe Veicolo
+         creare una classe Auto : Veicolo
+         creare una classe Fiesta : Auto
+         creare una classe Panda : Auto
+
+         creare una classe Moto : Veicolo
+         creare una classe Aprilia : Moto
+         creare una classe Ducati : Moto
+
+         per queste classi:
+            - definire delle properties e metodi arbitrari
+            - definire il livello di accessibilità (public, protected, private)
+            - definire per ciascun membro se è normale, virtual o abstract
+         istanziare un oggetto per ciascuna classe
+         valorizzare le property di ciascun oggetto
+         eseguire i metodi di ciascun oggetto
+         */
         static void Main(string[] args)
         {
-            Carlino ca = new Carlino();
-            ca.Razza = "";
-            ca.Altezza = "";
-            Stampa(ca);
 
-            Bassotto ba = new Bassotto();
-            ba.Razza = "";
-            ba.Altezza = "";
-            Stampa(ba);
         }
+        //static void Main(string[] args)
+        //{
+        //    Carlino ca = new Carlino();
+        //    ca.Razza = "";
+        //    ca.Altezza = "";
+        //    Stampa(ca);
 
-        public static void Stampa(Animale animale)
-        {
-            animale.FaiIlVerso();
-        }
+        //    Bassotto ba = new Bassotto();
+        //    ba.Razza = "";
+        //    ba.Altezza = "";
+        //    Stampa(ba);
+        //}
+
+        //public static void Stampa(Animale animale)
+        //{
+        //    animale.FaiIlVerso();
+        //}
     }
 }
