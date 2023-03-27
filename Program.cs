95a0d249a83b7c82dddf4d62cf605a4f75cea2d2
trippone trippini <visualtrippa@hotmail.com>
2023-03-27 20:51:55 +0200

    wqe

--- Program.cs
+++ Program.cs
@@ -1,11 +1,24 @@
 ﻿using ConsoleApplicationsTDPC14.Animali;
 using ConsoleApplicationsTDPC14.Veicoli;
 using System;
+using System.Collections.Generic;
 
 namespace ConsoleApplicationsTDPC14
 {
     internal class Program
     {
+        /*
+         simulare una libreria.
+            es: classe Libreria con all'interno una collection di Scaffali..
+                classe Scaffale con all'interno una collection di Libri..
+                classe Libro con properties Autore, Anno..
+         una volta definite le classi instanziare un oggetto di tipo Libreria
+         con n oggetti al suo interno
+         */
+        static void Main(string[] args)
+        {
+
+        }
         /*
          creare una classe Veicolo
          creare una classe Auto : Veicolo
@@ -24,51 +37,51 @@ namespace ConsoleApplicationsTDPC14
          valorizzare le property di ciascun oggetto
          eseguire i metodi di ciascun oggetto
          */
-        static void Main(string[] args)
-        {
-            Fiesta fiesta = new Fiesta();
-            fiesta.Carburante = "";
-            fiesta.Cilindrata = 2000;
-            fiesta.NumeroRuote = 4;
-            fiesta.NumeroPorte = 5;
-            fiesta.Accendi();
-            fiesta.InserisciFrenoAMano();
+        //static void Main(string[] args)
+        //{
+        //    Fiesta fiesta = new Fiesta();
+        //    fiesta.Carburante = "";
+        //    fiesta.Cilindrata = 2000;
+        //    fiesta.NumeroRuote = 4;
+        //    fiesta.NumeroPorte = 5;
+        //    fiesta.Accendi();
+        //    fiesta.InserisciFrenoAMano();
 
-            Panda panda = new Panda();
-            panda.Carburante = "";
-            panda.Cilindrata = 2000;
-            panda.NumeroRuote = 4;
-            panda.NumeroPorte = 5;
-            panda.Accendi();
-            panda.InserisciFrenoAMano();
+        //    Panda panda = new Panda();
+        //    panda.Carburante = "";
+        //    panda.Cilindrata = 2000;
+        //    panda.NumeroRuote = 4;
+        //    panda.NumeroPorte = 5;
+        //    panda.Accendi();
+        //    panda.InserisciFrenoAMano();
 
-            Aprilia aprilia = new Aprilia();
-            aprilia.Carburante = "";
-            aprilia.Cilindrata = 1500;
-            aprilia.NumeroRuote = 2;
-            aprilia.TipoSella = "";
-            aprilia.Accendi();
-            aprilia.Impenna();
+        //    Aprilia aprilia = new Aprilia();
+        //    aprilia.Carburante = "";
+        //    aprilia.Cilindrata = 1500;
+        //    aprilia.NumeroRuote = 2;
+        //    aprilia.TipoSella = "";
+        //    aprilia.Accendi();
+        //    aprilia.Impenna();
 
-            Ducati ducati = new Ducati();
-            ducati.Carburante = "";
-            ducati.Cilindrata = 1500;
-            ducati.NumeroRuote = 2;
-            ducati.TipoSella = "";
-            ducati.BrevettoSuono = "";
-            ducati.Accendi();
-            ducati.Impenna();
+        //    Ducati ducati = new Ducati();
+        //    ducati.Carburante = "";
+        //    ducati.Cilindrata = 1500;
+        //    ducati.NumeroRuote = 2;
+        //    ducati.TipoSella = "";
+        //    ducati.BrevettoSuono = "";
+        //    ducati.Accendi();
+        //    ducati.Impenna();
 
-            AccendiVeicolo(fiesta);
-            AccendiVeicolo(panda);
-            AccendiVeicolo(aprilia);
-            AccendiVeicolo(ducati);
-        }
+        //    AccendiVeicolo(fiesta);
+        //    AccendiVeicolo(panda);
+        //    AccendiVeicolo(aprilia);
+        //    AccendiVeicolo(ducati);
+        //}
 
-        public static void AccendiVeicolo(Veicolo veicolo)
-        {
-            veicolo.Accendi();
-        }
+        //public static void AccendiVeicolo(Veicolo veicolo)
+        //{
+        //    veicolo.Accendi();
+        //}
         //static void Main(string[] args)
         //{
         //    Carlino ca = new Carlino();
