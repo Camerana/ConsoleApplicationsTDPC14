4598ea4f216a20eab657483d5ea666e062590968
trippone trippini <visualtrippa@hotmail.com>
2023-03-27 20:31:45 +0200

    aef

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using ConsoleApplicationsTDPC14.Animali;
+using ConsoleApplicationsTDPC14.Veicoli;
 using System;
 
 namespace ConsoleApplicationsTDPC14
@@ -6,7 +7,6 @@ namespace ConsoleApplicationsTDPC14
     internal class Program
     {
         /*
-         * aggiornamento alle 7.50
          creare una classe Veicolo
          creare una classe Auto : Veicolo
          creare una classe Fiesta : Auto
@@ -26,7 +26,38 @@ namespace ConsoleApplicationsTDPC14
          */
         static void Main(string[] args)
         {
+            Fiesta fiesta = new Fiesta();
+            fiesta.Carburante = "";
+            fiesta.Cilindrata = 2000;
+            fiesta.NumeroRuote = 4;
+            fiesta.NumeroPorte = 5;
+            fiesta.Accendi();
+            fiesta.InserisciFrenoAMano();
 
+            Panda panda = new Panda();
+            panda.Carburante = "";
+            panda.Cilindrata = 2000;
+            panda.NumeroRuote = 4;
+            panda.NumeroPorte = 5;
+            panda.Accendi();
+            panda.InserisciFrenoAMano();
+
+            Aprilia aprilia = new Aprilia();
+            aprilia.Carburante = "";
+            aprilia.Cilindrata = 1500;
+            aprilia.NumeroRuote = 2;
+            aprilia.TipoSella = "";
+            aprilia.Accendi();
+            aprilia.Impenna();
+
+            Ducati ducati = new Ducati();
+            ducati.Carburante = "";
+            ducati.Cilindrata = 1500;
+            ducati.NumeroRuote = 2;
+            ducati.TipoSella = "";
+            ducati.BrevettoSuono = "";
+            ducati.Accendi();
+            ducati.Impenna();
         }
         //static void Main(string[] args)
         //{
---
+++ Veicoli/Veicoli.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApplicationsTDPC14.Veicoli
+{
+    public abstract class Veicolo
+    {
+        public int Cilindrata { get; set; }
+        public string Carburante { get; set; }
+        public int NumeroRuote { get; set; }
+        public abstract void Accendi();
+    }
+    public abstract class Auto : Veicolo
+    {
+        public int NumeroPorte { get; set; }
+        public void InserisciFrenoAMano() { }
+    }
+    public class Fiesta : Auto
+    {
+        public override void Accendi()
+        {
+            Console.WriteLine("La fiesta si accende");
+        }
+    }
+    public class Panda : Auto
+    {
+        public override void Accendi()
+        {
+            Console.WriteLine("La panda si accende");
+        }
+    }
+
+    public abstract class Moto : Veicolo
+    {
+        public string TipoSella { get; set; }
+        public void Impenna() { }
+    }
+    public class Aprilia : Moto
+    {
+        public override void Accendi()
+        {
+            Console.WriteLine("L'aprilia si accende");
+        }
+    }
+    public class Ducati : Moto
+    {
+        public string BrevettoSuono { get; set; }
+        public override void Accendi()
+        {
+            Console.WriteLine("La ducati si accende");
+        }
+    }
+}
