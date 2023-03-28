e467bf91ff954ee5583fb44f05bcf3886086263b
trippone trippini <visualtrippa@hotmail.com>
2023-03-28 20:36:07 +0200

    asd

--- Program.cs
+++ Program.cs
@@ -5,20 +5,34 @@ namespace ConsoleApplicationsTDPC14
 {
     internal class Program
     {
+        /*aggiornamento alle 21.00
+         - creare un'interfaccia IMangia con metodo Mangia
+         - creare una classe Persona che implementa IMangia
+         - creare una classe Alfiere che implementa IMangia
+
+         - creare in Program una funzione PrintMangia(IMangia oggetto)
+         che chiami il metodo Mangia di oggetto
+         - creare un oggetto di tipo Persona e un oggetto di tipo Alfiere
+         - passare entrambi gli oggetti alla funzione PrintMangia
+         */
         static void Main(string[] args)
         {
-            Bassotto bassotto = new Bassotto();
-            Labrador labrador = new Labrador();
-            UBoat uBoat = new UBoat();
-
-            Immersione(bassotto);
-            Immersione(labrador);
-            Immersione(uBoat);
 
         }
-        static void Immersione(IImmergiti oggetto)
-        {
-            oggetto.Immergiti();
-        }
+        //static void Main(string[] args)
+        //{
+        //    Bassotto bassotto = new Bassotto();
+        //    Labrador labrador = new Labrador();
+        //    UBoat uBoat = new UBoat();
+
+        //    Immersione(bassotto);
+        //    Immersione(labrador);
+        //    Immersione(uBoat);
+
+        //}
+        //static void Immersione(IImmergiti oggetto)
+        //{
+        //    oggetto.Immergiti();
+        //}
     }
 }
